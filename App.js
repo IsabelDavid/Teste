9b3daf04710fa041861049bac9e422c29b37336b
281291salma <raikanobre7@gmail.com>
2024-03-05 11:44:14 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Salma Diógenes</Text>
+      <Text>Isabel Camargo</Text>
+      <Text>05/03/2024</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
