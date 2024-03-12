9f05ccc3f298284691d99e652fbd937f2c8f133b
IsabelDavid <isabel.camargo@etec.sp.gov.br>
2024-03-12 12:04:32 -0300

    botão

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Salma Diógenes</Text>
       <Text>Isabel Camargo</Text>
       <Text>05/03/2024</Text>
+      <Button title='Salvar'></Button>
       <StatusBar style="auto" />
     </View>
   );
